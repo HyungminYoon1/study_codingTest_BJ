28b508aa4778d132d8468142dbe73bcd7e4a133b
HyungminYoon1 <156720686+HyungminYoon1@users.noreply.github.com>
2024-08-16 11:52:52 +0900

    saving solution records

--- src/day240809/No1934.java
+++ src/day240809/No1934.java
@@ -1,5 +1,25 @@
 package day240809;
 
-public class No1934 {
+import java.io.*;
 
+public class No1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String input = br.readLine();
+            int a = Integer.parseInt(input.split(" ")[0]);
+            int b = Integer.parseInt(input.split(" ")[1]);
+            bw.write(a*b/GCD(a,b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static int GCD(int a, int b) {
+        if (b == 0) return a;
+        return GCD(b, a % b);
+    }
 }
