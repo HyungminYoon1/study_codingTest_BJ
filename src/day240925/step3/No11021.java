833f388795e730d9413f324a78b889640155aba9
HyungminYoon1 <156720686+HyungminYoon1@users.noreply.github.com>
2024-09-25 14:49:24 +0900

    saving solution records

--- src/day240925/step3/No11021.java
+++ src/day240925/step3/No11021.java
@@ -1,21 +1,21 @@
-package day240924.step3;
+package day240925.step3;
 
 import java.io.*;
 
-public class No15552 {
+public class No11021 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             String inputStr = br.readLine();
             int A = Integer.parseInt(inputStr.split(" ")[0]);
             int B = Integer.parseInt(inputStr.split(" ")[1]);
-            bw.write((A+B) + "\n");
+            bw.write("Case #"+(i+1)+": " + (A+B)+"\n");
         }
-        br.close();
         bw.flush();
         bw.close();
+        br.close();
     }
 }
---
+++ src/day240925/step3/No2438.java
@@ -0,0 +1,21 @@
+package day240925.step3;
+
+import java.io.*;
+
+public class No2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i < N+1; i++) {
+            for (int j = 1; j < i+1; j++) {
+                bw.write("*");
+            }
+            if(i != N) bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
